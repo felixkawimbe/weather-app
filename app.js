b9bcfb76392cbabcbd7138340077b7dbe6736dc3
felixkawimbe <felixkawimbe@gmail.com>
2025-05-06 03:00:37 -0700

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const __dirname = path.dirname(__filename);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 //app.set('view engine', 'ejs');
-//app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
